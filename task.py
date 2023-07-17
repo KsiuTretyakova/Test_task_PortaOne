28b07ec4bf220ec2034e6e0f2536fe42e77d4946
KsiuTretyakova <hrytsenkoks@gmail.com>
2023-07-17 10:10:50 +0300

    add program and readme file

---
+++ task.py
@@ -0,0 +1,28 @@
+def word_processing():
+
+    print("Enter text for analysis:")
+
+    while True:
+        text = "\n".join(iter(input, ""))
+        if any(symbol.isalpha() for symbol in text) and len(text) > 0:
+            break
+        else:
+            print("The text must contain letters and not be empty. Try again")
+
+    text = text.split()
+
+    list_letter = []
+    for word in text:
+        for letter in word:
+            if word.count(letter) == 1 and letter.isalpha():
+                list_letter.append(letter)
+            break
+
+    for elem in list_letter:
+        if list_letter.count(elem) == 1:
+            print(elem)
+            break
+
+
+if __name__ == "__main__":
+    word_processing()
